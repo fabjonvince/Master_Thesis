81a7f05686c1ca9fc8ef0fd9a4e54569733f526a
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2023-04-05 10:21:12 +0200

    start woking on the creation of the graph

---
+++ src/data.py
@@ -0,0 +1,12 @@
+from datasets import load_dataset
+
+
+def get_dataset(dataset_name: str):
+    if dataset_name == 'eli5':
+        return load_dataset('eli5')
+    elif dataset_name == 'trivia_qa':
+        return load_dataset("trivia_qa")
+    elif dataset_name == 'web_questions':
+        return load_dataset("web_questions")
+    elif dataset_name == 'natural_questions':
+        return load_dataset("natural_questions")

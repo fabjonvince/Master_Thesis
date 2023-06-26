update 9.14 fixing backpropagation

src/t5.py
@@ -201,7 +201,6 @@ class CustomGNNLayer(torch.nn.Module):
         Returns:
             scores (list of lists): A list of lists containing the final scores for each embedding in each group.
         """
-        #pdb.set_trace()
 
         # Pad the groups to make them equally sized
         max_size = max(len(group) for group in k_nodes)
@@ -392,14 +391,19 @@ class CustomGNNLayer(torch.nn.Module):
                 all_scores.append(mean_emb)
             all_scores = torch.stack(all_scores).mean(dim=0)
             output.append(all_scores)
-        output = torch.stack(output)
-        output = self.gnn_reprj(output)
-        # Now I update the hidden states
         #pdb.set_trace()
-        hidden_states_clone = hidden_states.clone()  # Create a clone of hidden_states
-        hidden_states_clone[gnn_mask.bool()] += output  # Perform the element-wise addition
+
+        # Get the indices of ones in gnn_mask
+        ones_indices = torch.nonzero(gnn_mask == 1)
+        hidden_states_clone = hidden_states.clone()
+        for t, ids in zip(output, ones_indices):
+            outs = self.gnn_reprj(t)
+            t_exp = torch.zeros_like(hidden_states)
+            t_exp[0][ids[1]] = outs
+            hidden_states_clone += t_exp
+
         hidden_states = torch.autograd.Variable(
-            hidden_states_clone)  # Assign the modified clone back to hidden_states
+            hidden_states_clone)
 
         return hidden_states, current_reasoning_path
 